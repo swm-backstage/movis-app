ff00dd1e1e1cc9382055414986cafd836398db19
whitej00 <wlsdka12@gmail.com>
2024-08-12 15:04:38 +0900

    [feat] issue-#18: webview테스트

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { Platform } from "react-native";
 
-// const axiosHost = axios.create({
-//     baseURL: 'https://api.movis.klr.kr',
-//     withCredentials: true,
-// });
-
 const axiosHost = axios.create({
-  baseURL: Platform.OS === 'android' 
-  ? 'http://10.0.2.2:8080' 
-  : 'http://localhost:8080',
-  withCredentials: true,
+    baseURL: 'https://api.movis.klr.kr',
+    withCredentials: true,
 });
+
+// const axiosHost = axios.create({
+//   baseURL: Platform.OS === 'android' 
+//   ? 'http://10.0.2.2:8080/' 
+//   : 'http://localhost:8080',
+//   withCredentials: true,
+// });
 
 export default axiosHost;
--- src/screens/webview/WebViewScreen.tsx
+++ src/screens/webview/WebViewScreen.tsx
@@ -13,9 +13,10 @@ type WebViewScreenProps = StackScreenProps<
   typeof mainNavigations.WEBVIEW
 >;
 
-function WebViewScreen({ route }: WebViewScreenProps) {
+function WebViewScreen({ route, navigation }: WebViewScreenProps) {
   const { clubId } = route.params;
-  const webViewRef = useRef();
+  const [receivedData, setReceivedData] = useState('');
+  const webViewRef = useRef<WebView>(null);
   const [isCanGoBack, setIsCanGoBack] = useState(false);
 
   const onPressHardwareBackButton = () => {
@@ -35,12 +36,27 @@ function WebViewScreen({ route }: WebViewScreenProps) {
     }
   }, [isCanGoBack]);
 
+  useEffect(() => {
+    if (webViewRef.current && receivedData) {
+      console.log('React로 보낼 데이터: ', receivedData);
+      webViewRef.current.postMessage(receivedData);
+      navigation.navigate(mainNavigations.EVENT_CREATE, { clubId: clubId });
+    }
+  }, [receivedData, webViewRef.current]);
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
         ref={webViewRef}
         // source={{ uri: `https://movis.klr.kr/clubs/${clubId}` }}
         source={{ uri: `https://movis.klr.kr` }}
+        onMessage={({ nativeEvent: state }) => {
+          if (state.data === 'navigationStateChange') {
+            setIsCanGoBack(state.canGoBack);
+            return;
+          }
+          setReceivedData(state.data);
+        }}
         onLoadStart={() => webViewRef.current.injectJavaScript(`
           (function() {
             function wrap(fn) {
@@ -60,11 +76,6 @@ function WebViewScreen({ route }: WebViewScreenProps) {
       
           true;
         `)}
-        onMessage={({ nativeEvent: state }) => {
-          if (state.data === 'navigationStateChange') {
-            setIsCanGoBack(state.canGoBack);
-          }
-        }}
         style={styles.webview}
       />
     </SafeAreaView>
