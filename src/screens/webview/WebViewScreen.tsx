14099befcc3deee869e1553bda23dbbedf1f5a06
whitej00 <wlsdka12@gmail.com>
2024-08-16 17:05:48 +0900

    [feat] issue-#24: 웹뷰 통신 완료

--- src/screens/webview/WebViewScreen.tsx
+++ src/screens/webview/WebViewScreen.tsx
@@ -1,12 +1,36 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useEffect, useRef, useState } from 'react';
 import { BackHandler, Dimensions, SafeAreaView, StyleSheet } from 'react-native';
-import WebView from 'react-native-webview';
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
 import { mainNavigations } from '../../constants/navigations';
 import { MainStackParamList } from '../../navigations/MainStackNavigator';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+const injectedJavascript = `
+      (function() {
+        function wrap(fn) {
+          return function wrapper() {
+            var res = fn.apply(this, arguments);
+  
+            window.ReactNativeWebView.postMessage(JSON.stringify({
+              type: 'navigationStateChange'
+            }));
+            return res;
+          }
+        }
+  
+        history.pushState = wrap(history.pushState);
+        history.replaceState = wrap(history.replaceState);
+        window.addEventListener('popstate', function() {
+          window.ReactNativeWebView.postMessage(JSON.stringify({
+            type: 'navigationStateChange'
+          }));
+        });
+      })();
+  
+      true;
+    `
 
 type WebViewScreenProps = StackScreenProps<
   MainStackParamList,
@@ -15,19 +39,35 @@ type WebViewScreenProps = StackScreenProps<
 
 function WebViewScreen({ route, navigation }: WebViewScreenProps) {
   const { clubId } = route.params;
-  const [receivedData, setReceivedData] = useState('');
   const webViewRef = useRef<WebView>(null);
   const [isCanGoBack, setIsCanGoBack] = useState(false);
-
   const onPressHardwareBackButton = () => {
     if (webViewRef.current && isCanGoBack) {
       webViewRef.current.goBack();
       return true;
-    } 
+    }
     else {
       return false;
     }
   };
+  const onMessage = ({ nativeEvent: e }: WebViewMessageEvent) => {
+    const data = JSON.parse(e.data);
+    if (data.type === 'navigationStateChange') {
+      setIsCanGoBack(e.canGoBack);
+    } else if (data.type === 'eventCreate') {
+      navigation.navigate(mainNavigations.EVENT_CREATE, { clubId: data.clubId });
+    } else if (data.type === 'eventConfig') {
+      
+    }
+  };
+  const onLoad = () => {
+    if (webViewRef.current) {
+      webViewRef.current.injectJavaScript(injectedJavascript);
+      webViewRef.current.postMessage(JSON.stringify({
+        isChongmu: true,
+      }));
+    }
+  }
 
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', onPressHardwareBackButton);
@@ -36,52 +76,19 @@ function WebViewScreen({ route, navigation }: WebViewScreenProps) {
     }
   }, [isCanGoBack]);
 
-  useEffect(() => {
-    if (webViewRef.current && receivedData) {
-      console.log('React로 보낼 데이터: ', receivedData);
-      webViewRef.current.postMessage(receivedData);
-      navigation.navigate(mainNavigations.EVENT_CREATE, { clubId: clubId });
-    }
-  }, [receivedData, webViewRef.current]);
-
   return (
     <SafeAreaView style={styles.container}>
       <WebView
         ref={webViewRef}
-        // source={{ uri: `https://movis.klr.kr/clubs/${clubId}` }}
-        source={{ uri: `https://movis.klr.kr` }}
-        onMessage={({ nativeEvent: state }) => {
-          if (state.data === 'navigationStateChange') {
-            setIsCanGoBack(state.canGoBack);
-            return;
-          }
-          setReceivedData(state.data);
-        }}
-        onLoadStart={() => webViewRef.current.injectJavaScript(`
-          (function() {
-            function wrap(fn) {
-              return function wrapper() {
-                var res = fn.apply(this, arguments);
-                window.ReactNativeWebView.postMessage('navigationStateChange');
-                return res;
-              }
-            }
-      
-            history.pushState = wrap(history.pushState);
-            history.replaceState = wrap(history.replaceState);
-            window.addEventListener('popstate', function() {
-              window.ReactNativeWebView.postMessage('navigationStateChange');
-            });
-          })();
-      
-          true;
-        `)}
+        source={{ uri: `https://movis.klr.kr/clubs/${clubId}` }}
+        // source={{ uri: `http://10.0.2.2:3000/` }}
+        onMessage={onMessage}
+        onLoad={onLoad}
         style={styles.webview}
       />
     </SafeAreaView>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
