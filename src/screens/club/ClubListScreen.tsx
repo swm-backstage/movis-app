22bc00178e41f7b497fa01c487468c67aa9efcff
whitej00 <wlsdka12@gmail.com>
2024-11-08 23:06:47 +0900

    [feat] issue-#105: 메인 스크린에 입/출금 내역 생성 버튼만 표시(사유: 웹측에서 아직 웹뷰로 연결이 안됨

--- src/screens/club/ClubListScreen.tsx
+++ src/screens/club/ClubListScreen.tsx
@@ -78,10 +78,6 @@ function ClubListScreen({ navigation }: ClubHomeScreenProps) {
           handlePressWebView={handlePressWebView}>
         </ClubList>
         <ExpandedButton
-          onPress={() => navigation.navigate(mainNavigations.TRANSACTIONHISTORY_CREATE, { clubId: '01JC2STQWSKTV58E92XZTA3ZQC', eventId: '01JC56Q5F69VAF78WQMZ1K82KM' })}
-          buttonText='내역 생성(레거시)'
-        />
-        <ExpandedButton
           onPress={() => navigation.navigate(mainNavigations.TRANSACTIONHISTORY_DEPOSIT_CREATE, { clubId: '01JC2STQWSKTV58E92XZTA3ZQC', eventId: '01JC56Q5F69VAF78WQMZ1K82KM' })}
           buttonText='입금 내역 생성(테스트)'
         />
